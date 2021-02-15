49f20247ab6ef45b7e09c21042da4d67899a9419
tNahoko <nahoko.toyota@gmail.com>
2021-02-15 14:10:00 +0900

    create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log("started server on: ", port);
